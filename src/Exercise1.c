84176bcad69ccfb15387023174514769619b7492
NguyenBaPhucITITIU20278 <ITITIU20278@student.hcmiu.ed>
2021-10-26 01:40:41 +0700

    Update Exercise1.c

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -17,9 +17,20 @@ ______________________________________
 
 void Ex1(int n){
 	//Your codes here
-    
+ int a[20][20];
+for (int i = 0; i <= n; i++){
+for (int j = 0; j <= i; j++){ 
+	if (i == j || j == 0) {
+	a[i][j] = 1;
+	} 
+	else 
+	{ a[i][j]= a[i-1][j-1]+ a[i-1][j];
+	}
+	printf("%d ", a[i][j]);
+	}
+	printf("\n");
+  }
 }
-
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
